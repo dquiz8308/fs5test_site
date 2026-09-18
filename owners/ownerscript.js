9b3afb50b0732f729592be164ac4b5ab68d6cea3
wmeyer988 <wmeyer988@gmail.com>
2026-09-18 13:28:45 -0400

    Map R.A.N.D.O.M bowls to dancer collectible

--- owners/ownerscript.js
+++ owners/ownerscript.js
@@ -370,7 +370,7 @@ document.addEventListener('DOMContentLoaded', function() {
         if (bowl.indexOf('city of homes') !== -1) return { label: 'Welcome to Newnan Sign', type: 'city-of-homes', asset: 'artwork/locker/objects/newnan-city-of-homes-sign.png' };
         if (bowl.indexOf('battle of the bulge') !== -1) return { label: 'Bulge Tank', type: 'battle-tank', asset: 'artwork/locker/objects/battle-tank.png' };
         if (bowl.indexOf('jordan lynch') !== -1) return { label: 'Rusted Stadium Sign', type: 'lynch-football', asset: 'artwork/locker/objects/lynch-rusted-sign.png' };
-        if (bowl.indexOf('random') !== -1) return { label: 'R.A.N.D.O.M. Dancer', type: 'random-dancer', asset: 'artwork/locker/objects/random-dancer.png' };
+        if (bowl.indexOf('random') !== -1 || bowl.indexOf('r.a.n.d.o.m') !== -1) return { label: 'R.A.N.D.O.M. Dancer', type: 'random-dancer', asset: 'artwork/locker/objects/random-dancer.png' };
         if (bowl.indexOf('edge') !== -1) return { label: 'Edge Seat', type: 'edge-seat', asset: 'artwork/locker/objects/edge-seat.png' };
         if (bowl.indexOf('memorial') !== -1) return { label: 'Memorial Frame', type: 'memorial', asset: 'artwork/locker/objects/memorial.png' };
         if (bowl.indexOf('crosby') !== -1) return { label: 'Studio Reel', type: 'studio-reel', asset: 'artwork/locker/objects/studio-reel.png' };
